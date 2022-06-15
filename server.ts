420df4c61e58ac06848b2853d74aefafc393b4a2
Krzysztof Węgrzyn <k.wegrzyn@clearcode.cc>
2022-06-15 10:29:47 +0200

    Game instance settings are available in service container

--- server.ts
+++ server.ts
@@ -1,15 +1,17 @@
 import "reflect-metadata";
-import { ApolloServer } from "apollo-server";
+import {ApolloServer} from "apollo-server";
 import * as path from "path";
-import { buildSchema } from "type-graphql";
-import { PrismaClient } from "@prisma/client";
-import { Container } from "typedi";
+import {buildSchema} from "type-graphql";
+import {PrismaClient} from "@prisma/client";
+import {Container} from "typedi";
+import {ApolloContext} from "./src/ApolloContext";
+import {resolvers} from "./src/Resolvers";
 
-import { ApolloContext } from "./src/ApolloContext";
-import { resolvers } from "./src/Resolvers";
+const config = require('config');
 
 const prisma = new PrismaClient();
-Container.set({ id: "PRISMA", factory: () => prisma });
+Container.set({id: "PRISMA", factory: () => prisma});
+Container.set({id: "CONFIG", factory: () => config});
 
 async function bootstrap() {
     // build TypeGraphQL executable schema
