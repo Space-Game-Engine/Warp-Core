cc9097e88dba064168c60c11dab2268902f44eb9
Krzysztof Węgrzyn <k.wegrzyn@clearcode.cc>
2023-01-31 20:52:24 +0100

    Habitat model is compatible with auth module

--- src/habitat/model/habitat.model.ts
+++ src/habitat/model/habitat.model.ts
@@ -1,12 +1,13 @@
 import { Field, ID, ObjectType } from "@nestjs/graphql";
 import { IsBoolean, IsNumber } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { AuthModelInterface } from "../../auth/interface/auth-model.interface";
 import { BuildingQueueElementModel } from "../../building-queue/model/building-queue-element.model";
 import { BuildingZoneModel } from "../../building-zone/model/building-zone.model";
 
 @ObjectType({ description: "Single habitat that belongs to user" })
 @Entity({ name: "habitat" })
-export class HabitatModel {
+export class HabitatModel implements AuthModelInterface {
     @Field(type => ID)
     @IsNumber()
     @PrimaryGeneratedColumn()
@@ -30,5 +31,9 @@ export class HabitatModel {
     buildingZones: BuildingZoneModel[];
 
     @Field(type => [BuildingQueueElementModel])
-    buildingQueue: BuildingQueueElementModel[]
+    buildingQueue: BuildingQueueElementModel[];
+
+    getAuthId(): any {
+        return this.id;
+    }
 }
