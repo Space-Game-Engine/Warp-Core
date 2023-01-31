90005c3f36d1da78903a6599de3221d0d1e28bc7
Krzysztof Węgrzyn <k.wegrzyn@clearcode.cc>
2023-01-31 20:51:27 +0100

    Swagger installed into application

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Warp Core web game engine')
+    .setDescription('Web game engine REST API points')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
   console.log("App is loaded at " + (await app.getUrl()) + "/graphql URL");
 }
