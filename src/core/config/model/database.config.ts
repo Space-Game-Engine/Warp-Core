db46bcfa4282e000eb0a2bd7cf24aa71361cd53f
Krzysztof Wegrzyn <ptyskju@gmail.com>
2023-03-31 16:50:11 +0200

    Database connection config is moved to config yaml

---
+++ src/core/config/model/database.config.ts
@@ -0,0 +1,29 @@
+import { Type } from "@nestjs/common"
+import { IsBoolean, IsString } from "class-validator"
+import { DatabaseType } from "typeorm"
+
+export class DatabaseConfig {
+
+    @IsString()
+    type: DatabaseType
+
+    @IsString()
+    host?: string
+
+    @IsString()
+    port?: string
+
+    @IsString()
+    username?: string
+
+    @IsString()
+    password?: string
+
+    @IsString()
+    database: string
+
+    @IsBoolean()
+    synchronize: boolean
+
+    entities: Type[];
+}
