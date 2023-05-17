5b901b3c608011e3fff7f03f6e7da3e8f3476e6c
Krzysztof Wegrzyn <ptyskju@gmail.com>
2023-05-17 15:22:25 +0200

    Field type variable fix

--- src/building-queue/input/add-to-queue.input.ts
+++ src/building-queue/input/add-to-queue.input.ts
@@ -7,13 +7,13 @@ export class AddToQueueInput
 {
     @IsNumber({}, {message: 'Building zone id must be a number'})
     @IsPositive({ message: 'Building zone id must be a positive number' })
-    @Field(type => Int,{description: "Local Id of building zone"})
+    @Field(() => Int,{description: "Local Id of building zone"})
     localBuildingZoneId: number;
 
     @IsOptional()
     @IsNumber()
     @IsPositive()
-    @Field(type => Int, {
+    @Field(() => Int, {
         description: "Id of building type that will be constructed. If building is already placed, that field will be ignored",
         nullable: true,
     })
@@ -21,6 +21,6 @@ export class AddToQueueInput
 
     @IsNumber()
     @IsPositive()
-    @Field(type => Int, { description: "How much levels will be build" })
+    @Field(() => Int, { description: "How much levels will be build" })
     endLevel: number;
 }
