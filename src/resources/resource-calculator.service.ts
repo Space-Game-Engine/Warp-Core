205613b35747253aa8a6f7e21d9cd59ce4978160
Krzysztof Wegrzyn <ptyskju@gmail.com>
2023-05-26 15:44:00 +0200

    Calculating resources after queue update Customizable calculation time

--- src/resources/resource-calculator.service.ts
+++ src/resources/resource-calculator.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { OnEvent } from "@nestjs/event-emitter";
 import { AuthorizedHabitatModel } from "@warp-core/auth/payload/model/habitat.model";
+import { QueueElementAfterProcessingEvent } from "@warp-core/building-queue/event/queue-element-after-processing.event";
 import { BuildingZoneModel } from "@warp-core/database/model/building-zone.model";
 import { HabitatResourceModel } from "@warp-core/database/model/habitat-resource.model";
 import { BuildingZoneRepository } from "@warp-core/database/repository/building-zone.repository";
+import { HabitatResourceRepository } from "@warp-core/database/repository/habitat-resource.repository";
 import { DateTime } from "luxon";
 
 @Injectable()
@@ -10,10 +13,11 @@ export class ResourceCalculatorService {
 
     constructor(
         private readonly buildingZoneRepository: BuildingZoneRepository,
+        private readonly habitatResourceRepository: HabitatResourceRepository,
         private readonly habitatModel: AuthorizedHabitatModel,
     ) {}
 
-    async calculateSingleResource(habitatResource: HabitatResourceModel) {
+    async calculateSingleResource(habitatResource: HabitatResourceModel, calculationEndTime: Date = new Date()) {
         const resource = await habitatResource.resource;
 
         const buildingZones = await this.buildingZoneRepository.getBuildingZonesForOneResource(
@@ -22,15 +26,39 @@ export class ResourceCalculatorService {
         );
 
         for (const singleBuildingZone of buildingZones) {
-            await this.calculateResourceForBuildingZone(habitatResource, singleBuildingZone);
+            await this.calculateResourceForBuildingZone(habitatResource, singleBuildingZone, calculationEndTime);
         }
     }
 
-    private async calculateResourceForBuildingZone(habitatResource: HabitatResourceModel, buildingZone: BuildingZoneModel) {
+    @OnEvent('building_queue.after_processing_element')
+    async calculateAfterQueueUpdate(beforeProcessingEvent: QueueElementAfterProcessingEvent) {
+        const buildingQueueElement = beforeProcessingEvent.queueElement;
+        const building = await buildingQueueElement.building;
+        const buildingDetails = (await building.buildingDetailsAtCertainLevel)
+            .find((singleDetails) => {
+                return singleDetails.level == buildingQueueElement.endLevel;
+            });
+        
+        for (const productionRateDetails of await buildingDetails.productionRate) {
+            const habitatResource =  await this.habitatResourceRepository.findOneBy({
+                resourceId: productionRateDetails.resourceId,
+                habitatId: this.habitatModel.id
+            });
+
+            if (!habitatResource) {
+                continue;
+            }
+
+            this.calculateSingleResource(habitatResource, buildingQueueElement.endTime);
+            this.habitatResourceRepository.save(habitatResource);
+        }
+    }
+
+    private async calculateResourceForBuildingZone(habitatResource: HabitatResourceModel, buildingZone: BuildingZoneModel, calculationEndTime: Date) {
         const productionRate = await this.getProductionRateForSingleBuildingZone(buildingZone);
         const lastlyCalculatedAmount = habitatResource.currentAmount;
         const lastCalculationTime = DateTime.fromJSDate(habitatResource.lastCalculationTime);
-        const timeUntilNowInSeconds = lastCalculationTime.diffNow('seconds').as('seconds');
+        const timeUntilNowInSeconds = lastCalculationTime.diff(DateTime.fromJSDate(calculationEndTime)).as('seconds');
 
         const currentAmount = lastlyCalculatedAmount + (productionRate * Math.abs(timeUntilNowInSeconds));
 
