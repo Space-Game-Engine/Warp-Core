83743708c5d0e8c1cbd8488e2456a9b8ed18b26e
Krzysztof Wegrzyn <ptyskju@gmail.com>
2023-07-31 17:08:10 +0200

    Modified node js action to test code

--- src/core/install/service/abstract-instalator.service.ts
+++ src/core/install/service/abstract-instalator.service.ts
@@ -1,4 +1,4 @@
-import {Type} from '@nestjs/common';
+import {Logger, Type} from '@nestjs/common';
 import {plainToInstance} from 'class-transformer';
 import {validateSync} from 'class-validator';
 
@@ -6,6 +6,8 @@ export function AbstractInstalatorService<T extends Type<unknown>>(
 	classRef: T,
 ): any {
 	abstract class AbstractInstalatorServiceClass {
+
+		protected static readonly logger: Logger = new Logger('installation');
 		async install(arrayToInstall: any) {
 			for (const key in arrayToInstall) {
 				if (
@@ -29,7 +31,8 @@ export function AbstractInstalatorService<T extends Type<unknown>>(
 				return true;
 			}
 
-			console.error('Validation error', validationErrors);
+			AbstractInstalatorServiceClass.logger.error('Validation error');
+			AbstractInstalatorServiceClass.logger.error(validationErrors);
 			throw new Error('Validation error, see logs');
 		}
 
