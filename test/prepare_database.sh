448bbb275188d8b72325e5f30d6fce08c157fb72
Krzysztof Wegrzyn <ptyskju@gmail.com>
2023-09-06 17:32:19 +0200

    Added e2e test into workflows

--- test/prepare_database.sh
+++ test/prepare_database.sh
@@ -6,3 +6,12 @@ npm run typeorm schema:drop -- -d "$DATA_SOURCE_FILE"
 npm run typeorm schema:sync -- -d "$DATA_SOURCE_FILE"
 npm run typeorm migration:run -- -d "$DATA_SOURCE_FILE"
 npm run install-game
+
+export FILE=test_db.db
+
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+    echo "$FILE does not exist."
+    exit 1
+fi
