1d4018e9ecaf379f412a179c9179e6603f2feb28
Krzysztof Wegrzyn <krzysztof.wegrzyn@flip.shop>
2025-03-01 18:13:28 +0100

    Event emitter in internal exchange module

--- src/core/utils/internal-exchange/index.ts
+++ src/core/utils/internal-exchange/index.ts
@@ -1,5 +1,6 @@
 export * from './internal-exchange.module';
 export * from './internal-exchange-query.decorator';
+export * from './internal-exchange-emit.decorator';
 export * from './internal-exchange.emitter';
 export * from './type';
 export * from './error';
