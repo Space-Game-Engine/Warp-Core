986e7237716dd1597bde251bdeb199a3d2b042fa
Krzysztof Wegrzyn <krzysztof.wegrzyn@flip.shop>
2025-03-25 16:34:32 +0100

    Adding exchange listeners

--- src/user/resources/exchange/subscriber/resources-query.subscriber.ts
+++ src/user/resources/exchange/subscriber/resources-query.subscriber.ts
@@ -3,10 +3,10 @@ import {Injectable} from '@nestjs/common';
 import {InternalExchangeQuery} from '@warp-core/core/utils/internal-exchange';
 import {HabitatResourceCombined} from '@warp-core/database/model/habitat-resource.mapped.model';
 import {ResourcesQueryNames} from '@warp-core/user/resources/exchange/query/resources-query.names';
-import {ResourcesService} from '@warp-core/user/resources/resources.service';
+import {ResourcesService} from '@warp-core/user/resources/service/resources.service';
 
 @Injectable()
-export class ResourcesQueryHandler {
+export class ResourcesQuerySubscriber {
 	constructor(private readonly resourceService: ResourcesService) {}
 
 	@InternalExchangeQuery(ResourcesQueryNames.GetResourcesPerHabitat)
--- src/user/resources/resources.module.ts
+++ src/user/resources/resources.module.ts
@@ -4,31 +4,35 @@ import {AuthModule} from '@warp-core/auth';
 import {CoreConfigModule} from '@warp-core/core/config/core-config.module';
 import {AddInstallService} from '@warp-core/core/install/add-install-service.decorator';
 import {DatabaseModule} from '@warp-core/database/database.module';
-import {ResourceCalculatorService} from '@warp-core/user/resources/calculate/resource-calculator.service';
-import {CalculateResourceStorageService} from '@warp-core/user/resources/calculate/warehouse-storage/calculate-resource-storage.service';
-import {ResourcesQueryHandler} from '@warp-core/user/resources/exchange/query/resources-query.handler';
+import {HabitatResourceRecalculateSubscriber} from '@warp-core/user/resources/database-subscriber/habitat-resource-recalculate.subscriber';
+import {NewHabitatSubscriber} from '@warp-core/user/resources/exchange/subscriber/new-habitat.subscriber';
+import {QueueProcessingSubscriber} from '@warp-core/user/resources/exchange/subscriber/queue-processing.subscriber';
+import {ResourcesQuerySubscriber} from '@warp-core/user/resources/exchange/subscriber/resources-query.subscriber';
 import {ResourcesInstallService} from '@warp-core/user/resources/install/resources-install.service';
-import {QueueResourceExtractorService} from '@warp-core/user/resources/queue-resource-extractor.service';
 import {ResourcesResolver} from '@warp-core/user/resources/resources.resolver';
-import {ResourcesService} from '@warp-core/user/resources/resources.service';
-import {AddResourcesOnFirstHabitatSubscriber} from '@warp-core/user/resources/subscriber/add-resources-on-first-habitat.subscriber';
-import {CreateResourcesPerHabitat} from '@warp-core/user/resources/subscriber/create-resources-per-habitat.subscriber';
-import {HabitatHasNewResourceProducerSubscriber} from '@warp-core/user/resources/subscriber/habitat-has-new-resource-producer.subscriber';
-import {HabitatResourceRecalculateSubscriber} from '@warp-core/user/resources/subscriber/habitat-resource-recalculate.subscriber';
+import {AddResourcesOnFirstHabitatService} from '@warp-core/user/resources/service/add-resources-on-first-habitat.service';
+import {ResourceCalculatorService} from '@warp-core/user/resources/service/calculate/resource-calculator.service';
+import {CalculateResourceStorageService} from '@warp-core/user/resources/service/calculate/warehouse-storage/calculate-resource-storage.service';
+import {CreateResourcesPerHabitatService} from '@warp-core/user/resources/service/create-resources-per-habitat.service';
+import {HabitatHasNewResourceProducerService} from '@warp-core/user/resources/service/habitat-has-new-resource-producer.service';
+import {QueueResourceExtractorService} from '@warp-core/user/resources/service/queue-resource-extractor.service';
+import {ResourcesService} from '@warp-core/user/resources/service/resources.service';
 
 @Module({
 	providers: [
-		CreateResourcesPerHabitat,
+		CreateResourcesPerHabitatService,
 		HabitatResourceRecalculateSubscriber,
-		HabitatHasNewResourceProducerSubscriber,
+		HabitatHasNewResourceProducerService,
 		ResourcesService,
 		ResourceCalculatorService,
 		QueueResourceExtractorService,
 		ResourcesResolver,
 		CalculateResourceStorageService,
-		AddResourcesOnFirstHabitatSubscriber,
+		AddResourcesOnFirstHabitatService,
 		ResourcesInstallService,
-		ResourcesQueryHandler,
+		ResourcesQuerySubscriber,
+		NewHabitatSubscriber,
+		QueueProcessingSubscriber,
 	],
 	imports: [DatabaseModule, AuthModule, CoreConfigModule],
 	exports: [ResourcesService, ResourcesInstallService],
