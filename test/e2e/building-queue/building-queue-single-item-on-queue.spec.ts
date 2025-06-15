e4a908a8c40613699d02df79188c192f7283f0d5
Krzysztof Węgrzyn <k.wegrzyn@piwik.pro>
2025-06-15 18:06:32 +0200

    More building queue test

--- test/e2e/building-queue/building-queue-single-item-on-queue.spec.ts
+++ test/e2e/building-queue/building-queue-single-item-on-queue.spec.ts
@@ -50,5 +50,66 @@ describe('Building queue with single item on queue', () => {
 			expect(response.body.data.buildingQueue_add.endLevel).toEqual(2);
 			expect(response.body.data.buildingQueue_add.isConsumed).toEqual(false);
 		});
+
+		it('should throw error when user wants to update by multiple levels when update is possible', async () => {
+			const response = await requestTest
+				.mutation({
+					root: 'buildingQueue_add',
+					operationName: 'AddToQueue',
+					fields: {
+						fields: ['startLevel', 'endLevel', 'isConsumed'],
+					},
+					variables: {
+						addToQueue: {
+							type: 'AddToQueueInput',
+							value: {
+								localBuildingZoneId: 2,
+								endLevel: 4,
+							},
+						},
+					},
+				})
+				.send()
+				.expect(HttpStatus.OK);
+
+			expect(response.body.errors).toHaveLength(1);
+			expect(response.body.errors[0].message).toBe('Queue Validation Error');
+			expect(response.body.errors[0].validationError.endLevel).toHaveLength(1);
+			expect(response.body.errors[0].validationError.endLevel).toContain(
+				'You can only upgrade a building by one level at a time',
+			);
+		});
+
+		it('should throw error when user wants to update by multiple levels and when update for that level is not possible', async () => {
+			const response = await requestTest
+				.mutation({
+					root: 'buildingQueue_add',
+					operationName: 'AddToQueue',
+					fields: {
+						fields: ['startLevel', 'endLevel', 'isConsumed'],
+					},
+					variables: {
+						addToQueue: {
+							type: 'AddToQueueInput',
+							value: {
+								localBuildingZoneId: 1,
+								endLevel: 4,
+							},
+						},
+					},
+				})
+				.send()
+				.expect(HttpStatus.OK);
+
+			expect(response.body.errors).toHaveLength(1);
+			expect(response.body.errors[0].message).toBe('Queue Validation Error');
+			expect(response.body.errors[0].validationError.endLevel).toHaveLength(2);
+			expect(response.body.errors[0].validationError.endLevel).toContain(
+				'You can only upgrade a building by one level at a time',
+			);
+			expect(response.body.errors[0].validationError.endLevel).toContain(
+				'You cannot update higher than it is possible. Check Building update details.',
+			);
+		});
 	});
 });
