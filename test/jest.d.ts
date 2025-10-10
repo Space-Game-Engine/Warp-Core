efec758009962eb99f95b36ba436a3176fa66e2b
Krzysztof Węgrzyn <ptyskju@gmail.com>
2025-10-10 20:18:08 +0200

    Add new test matcher

--- test/jest.d.ts
+++ test/jest.d.ts
@@ -1,12 +1,17 @@
 import {
 	ResourceCheck,
+	ResourceWithCustomPropertyCheck,
+	toHaveResource,
+	toHaveResourceWithCustomProperty,
 	toHaveResourceWithValue,
 } from '@warp-core/test/expect-extend/resource-assert';
 
 declare global {
 	namespace jest {
 		interface Matchers<R> {
+			toHaveResource(expected: {id: string}): R
 			toHaveResourceWithValue(expected: ResourceCheck): R
+			toHaveResourceWithCustomProperty(expected: ResourceWithCustomPropertyCheck): R
 		}
 	}
 }
